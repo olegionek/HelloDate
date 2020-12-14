f2414a18e9f36af12421bcceca9dfb0d424bf6c5
olegionek <olegionek@gmail.com>
2020-12-14 21:40:07 +0300

    Initial commit

---
+++ src/HelloDate.java
@@ -0,0 +1,22 @@
+
+//:object/HelloDate.java
+
+import java.util.*;
+
+/** Первая программа - пример книги.
+ * Выводит строку и текущее число
+ * @author Bruse Ekkel
+ * @author www.MindView.com
+ * @version 4.0
+ */
+public class HelloDate {
+/** Точка входа в класс и приложение
+ * @param args Массив строковых инструментов
+ * @throws exceptions Исключения не выдаются
+ */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
+/* Output: (55% match)
+*///
---
+++ src/example.java
@@ -0,0 +1,20 @@
+/**
+ * @author HOME
+ *
+ */
+/**
+ * @author HOME
+ *
+ */
+public class example {
+	static public class ATypeName{
+		String a="Hello World";
+		int s=66;
+	}
+	
+	public static void main(String[] args) {
+		ATypeName data = new ATypeName();
+		System.out.println(data.a);
+		System.out.println(data.s);
+	}
+}
