85458268193397c40313df58fc314d043cab8a20
olegionek <olegionek@gmail.com>
2020-12-14 22:30:09 +0300

    Update HelloDate.java

--- src/HelloDate.java
+++ src/HelloDate.java
@@ -3,19 +3,13 @@
 
 import java.util.*;
 
-/** Первая программа - пример книги.
- * Выводит строку и текущее число
- * @author Bruse Ekkel
- * @author www.MindView.com
- * @version 4.0
- */
 public class HelloDate {
-/** Точка входа в класс и приложение
- * @param args Массив строковых инструментов
- * @throws exceptions Исключения не выдаются
- */
+
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
+		int storage(String s) {
+			return s.length();
+		}
 	}
 }
 /* Output: (55% match)
