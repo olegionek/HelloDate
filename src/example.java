5bf586c20643b3f5d424396ab5b82a4aacac08f2
olegionek <olegionek@gmail.com>
2020-12-14 22:29:15 +0300

    Update example.java

--- src/example.java
+++ src/example.java
@@ -15,9 +15,11 @@ public class example {
 		}
 	}
 	
+	static
+	
 	public static void main(String[] args) {
+	
 		ATypeName data = new ATypeName();
 		System.out.println(data.a);
-		System.out.println(data.storage(string s));
 	}
 }
