141c58805ed3de86560b30a4114e9981b0aa66e0
olegionek <olegionek@gmail.com>
2020-12-14 22:04:41 +0300

    Update example.java

--- src/example.java
+++ src/example.java
@@ -7,14 +7,17 @@
  *
  */
 public class example {
+	
 	static public class ATypeName{
 		String a="Hello World";
-		int s=66;
+		int storage(String s) {
+			return s.length()*2;
+		}
 	}
 	
 	public static void main(String[] args) {
 		ATypeName data = new ATypeName();
 		System.out.println(data.a);
-		System.out.println(data.s);
+		System.out.println(data.storage(string s));
 	}
 }
