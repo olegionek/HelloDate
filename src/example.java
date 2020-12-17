dde5415af7f32e00b8c6410aa121450f7d91d067
olegionek <olegionek@gmail.com>
2020-12-17 15:38:45 +0300

    Update example.java

--- src/example.java
+++ src/example.java
@@ -1,25 +1,17 @@
-/**
- * @author HOME
- *
- */
-/**
- * @author HOME
- *
- */
 public class example {
-	
-	static public class ATypeName{
-		String a="Hello World";
-		int storage(String s) {
-			return s.length()*2;
-		}
-	}
-	
-	static
-	
+/**
+ * @author HOME
+ * @param args
+ * 
+ */
+
 	public static void main(String[] args) {
-	
-		ATypeName data = new ATypeName();
-		System.out.println(data.a);
+		int i = 1;
+		System.out.println("i : " + i);
+		System.out.println("++i : " + ++i);
+		System.out.println("i++ : " + i++);
+		System.out.println("i : " + i);
+		System.out.println("--i : " + --i);
+		System.out.println("i-- : " + i--);
 	}
 }
