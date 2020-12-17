4c62cf21dbaa9c93e46ac3dddbee3592ed0b0082
olegionek <olegionek@gmail.com>
2020-12-17 19:28:40 +0300

    Create Bool.java

---
+++ src/Bool.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+
+public class Bool {
+
+	public static class sides {
+		String a = "Orel";
+		String b = "Reshka";
+	}
+	
+	public static void main(String[] args) {
+		sides sides = new sides();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Выберите сторону Орел (a) или Решка (b) : ");
+		int input = scanner.nextInt();
+		if (input ) {
+			
+		}
+		Random rand = new Random();
+		
+	}
+
+	
+
+	
+}
+
