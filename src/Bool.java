8f35d695d20c079847ee0d04739d76dbcda592e9
olegionek <olegionek@gmail.com>
2020-12-20 21:42:52 +0300

    Update Bool.java

--- src/Bool.java
+++ src/Bool.java
@@ -4,24 +4,40 @@ import java.util.*;
 public class Bool {
 
 	public static class sides {
-		String a = "Orel";
-		String b = "Reshka";
+		Integer a = 1;
+		Integer b = 2;
+		int c;
 	}
+	
+	
 	
 	public static void main(String[] args) {
 		sides sides = new sides();
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Выберите сторону Орел (a) или Решка (b) : ");
-		int input = scanner.nextInt();
-		if (input ) {
-			
+		Random rnd = new Random();
+		
+		//generic random sides of monetka
+		rnd.nextInt();
+		
+		System.out.print("Should you go?");
+		String itor = scanner.nextLine();
+		
+		
+		
+		
+			System.out.println("Выберите сторону Орел (1) или Решка (2) : ");
+			String input = scanner.nextLine();
+			int i= Integer.parseInt(input);
+			if(i == rnd.nextInt(2)){
+				System.out.println("You win!");
 		}
-		Random rand = new Random();
+			else {
+			System.out.println("FaIL");
+		}
+		System.out.println(input);
 		
 	}
 
-	
-
-	
 }
+
 
