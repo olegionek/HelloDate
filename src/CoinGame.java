323b407ecc09ec16e7a8f7ef9cd78ad092147291
Olegionek <olegionek@gmail.com>
2021-06-15 19:08:37 +0300

    Clean code how i can now.

--- src/CoinGame.java
+++ src/CoinGame.java
@@ -1,27 +1,31 @@
 import java.util.*;
 
 
+
 public class CoinGame {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		Random rnd = new Random();
-		
-		//generic random sides of monetka
+		Random rnd = new Random(); //generic random sides of coin
 
-		rnd.nextInt();
+
 		System.out.print("Should you go?");
 		String itor = scanner.nextLine();
 
 		//Program Cycle
 		while (itor.equalsIgnoreCase("y"))// equalsIgnoreCase сверяет даже с текстом
 		{
+			rnd.nextInt();
 			System.out.println("Выберите сторону Орел (1) или Решка (2) : ");
 			String input = scanner.nextLine();
 			int i = Integer.parseInt(input);
-			if (i == rnd.nextInt(2)) {
+			if (i == rnd.nextInt(2)+1) {
 				System.out.println("You win!");
-			} else {
+			}
+			if (i==NumberFormatException){ //How to add exception?
+
+			}
+			else {
 				System.out.println("Fail");
 			}
 			System.out.println(input);
