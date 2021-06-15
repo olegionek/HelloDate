1272bdf18660310a5d851a20ac44d8b749e18c7c
Olegionek <olegionek@gmail.com>
2021-06-15 22:24:01 +0300

    Clean code how i can now.

--- src/CoinGame.java
+++ src/CoinGame.java
@@ -4,38 +4,42 @@ import java.util.*;
 
 public class CoinGame {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
 		Random rnd = new Random(); //generic random sides of coin
 
 
-		System.out.print("Should you go?");
+		System.out.print("Should you go?\n");
 		String itor = scanner.nextLine();
 
 		//Program Cycle
 		while (itor.equalsIgnoreCase("y"))// equalsIgnoreCase сверяет даже с текстом
 		{
-			rnd.nextInt();
-			System.out.println("Выберите сторону Орел (1) или Решка (2) : ");
-			String input = scanner.nextLine();
-			int i = Integer.parseInt(input);
-			if (i == rnd.nextInt(2)+1) {
-				System.out.println("You win!");
+			try{
+				rnd.nextInt();
+				System.out.println("Выберите сторону Орел (1) или Решка (2) : ");
+				String input = scanner.nextLine();
+				int i = Integer.parseInt(input);
+				if (i == rnd.nextInt(2)+1) {
+					System.out.println("You win!");
+				}
+				if (input == null) throw new Exception("1");//How to add exception?
+				else {
+					System.out.println("Fail");
+				}
+				System.out.println(input);
+				System.out.println("Продолжить? (y,n) ");
+				itor = scanner.nextLine();
 			}
-			if (i==NumberFormatException){ //How to add exception?
+			catch (Exception ex){
+				System.out.println(ex.getMessage());
 
-			}
-			else {
-				System.out.println("Fail");
-			}
-			System.out.println(input);
-			System.out.println("Продолжить? (y,n) ");
-			itor = scanner.nextLine();
+		}
 		}
 		System.out.println("До свидания!");
 
-	}
 
+}
 }
 
 
